6b4882f6c64846f96746ce3facc308396fc8c577
George Sun <Gsunems@gmail.com>
2020-05-09 15:33:02 -0500

    work on front end

---
+++ src/components/Details.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { ProductCard } from './ProductCard'
+
+export default function Details() {
+
+    return (
+        <div>
+            <h3>Hello from Details</h3>
+            <ProductCard title="Best Title Ever" />
+        </div>
+    );
+
+}
