0b0bb6b153be375ad6e876bd8daf3a0b76c49cbf
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-05-16 09:59:33 -0500

    Added initial Global State Management code. Still need to hook up.

---
+++ src/utils/actions.js
@@ -0,0 +1,5 @@
+// BOOKS
+export const GET_ALL_BOOKS = "GET_ALL_BOOKS";
+export const GET_CURRENT_BOOK = "GET_CURRENT_BOOK";
+export const ADD_BOOK = "ADD_BOOK";
+export const REMOVE_BOOK = "REMOVE_BOOK";
