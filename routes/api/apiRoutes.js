3147b952140d1e58c28324877e7fe96d872e7055
Ryan <ryanmagill97@gmail.com>
2020-05-23 15:34:15 -0500

    added api route to grab items from db once db models are created

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,19 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import App from '.../client/src/App.js'
-import cart from '.../client/src/components/Cart.js'
+const router = require('express').Router();
+let Item = require('../models/items.model');
 
+router.route('/items').get((req,res) => {
+  Item.find()
+    .then(items => res.json(items))
+    .catch(err => res.status(400).json('Error: '+err));
+});
 
-const routing = (
-  <Router>
-    <div>
-      <Route exact path ="/" component={App} />
-      <Route path ="/cart" component={cart} />
-    </div>
-  </Router>
-)
-
-
-ReactDOM.render(routing, document.getElementById('root'))
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,16 @@
 const path = require("path");
 const router = require("express").Router();
-<<<<<<< HEAD
 const apiRoutes = require("./api");
 
 // API Routes
 router.use("/api", apiRoutes);
-=======
 const passport = require("../passport");
 const routeHelper = require('./utils/routeHelper');
 
 
-//const apiRoutes = require("./api");
-
 // API Routes
-//router.use("/api", apiRoutes);
 /* OAuth Github Routes */
 router.get("/login", passport.authenticate('github'));
->>>>>>> 07103b016a93d86a2d9231c3f354e12f9d0a7ff7
 
 router.get('/auth', passport.authenticate('github', {session: false, failureRedirect: routeHelper()}), function(req, res) {
     // Succesful authentication! 
